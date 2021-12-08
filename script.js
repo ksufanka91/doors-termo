63104e250e5223da3b81b8a9a662f0adbebe2723
kseniia <ksufanka91@mail.ru>
2021-12-08 16:05:13 +0300

    add changes(pixelPerfect) to bonus, catalogue, chart, comments, description, footer, leaders, process, promo, shops-gallery, shops-map, splide, technology, top, visualization

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 let slider = new Splide('.splide', {
     perPage: 1,
     perMove: 1,
+    arrows: false,
     pagination: false,
     gap: '10px',
-
-    prevArrow: '<div class="splide__prev"></div>',
-    nextArrow: '<div class="splide__next"></div>',
-
 });
 
-if (window.innerWidth > 768) {
-    slider.mount();
-}
+slider.mount();
+
+document.querySelector('.splide__arrow_prev').addEventListener('click', function () {
+    slider.go('<');
+});
+
+document.querySelector('.splide__arrow_next').addEventListener('click', function () {
+    slider.go('>');
+});
