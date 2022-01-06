27a0920de82f1c4a84e49b0966a3d9c53211f628
kseniia <ksufanka91@mail.ru>
2022-01-06 14:57:30 +0300

    add script button to top add changes to timer add yandex map script

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// slider
 let slider = new Splide('.splide', {
     perPage: 1,
     perMove: 1,
@@ -16,27 +17,29 @@ document.querySelector('.splide__arrow_next').addEventListener('click', function
     slider.go('>');
 });
 
-
-function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date());
-    let seconds = Math.floor((t / 1000) % 60);
-    let minutes = Math.floor((t / 1000 / 60) % 60);
-    let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    return {
-        'total': t,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-    };
-}
-
-document.querySelector('.countdown__main-text').innerHTML = "Успей! Осталось " + " ";
-
+// timer
 function initializeClock(id, endtime) {
+    let intervalId;
     let clock = document.getElementById(id);
     let hoursSpan = clock.querySelector('.hours');
     let minutesSpan = clock.querySelector('.minutes');
     let secondsSpan = clock.querySelector('.seconds');
+
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());
+
+        let seconds = Math.floor((t / 1000) % 60);
+        let minutes = Math.floor((t / 1000 / 60) % 60);
+        let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+
+        return {
+            total: t,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        };
+    }
 
     function updateClock() {
         let t = getTimeRemaining(endtime);
@@ -45,14 +48,64 @@ function initializeClock(id, endtime) {
         minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
         secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
 
-        if (t.total <= 0) {
-            clearInterval(timeinterval);
+        if (t.total <= 0 && intervalId) {
+            clearInterval(intervalId);
         }
     }
 
     updateClock();
-    let timeinterval = setInterval(updateClock, 1000);
+    intervalId = setInterval(updateClock, 1000);
 }
 
-let deadline = "December 10 2021 00:00:00 GMT+0300";
-initializeClock('countdown', deadline);
+// для demo всегда отсчитываем 12 часов
+let today = new Date().getTime();
+let endtime = new Date(today + 12 * 60 * 60 * 1000);
+initializeClock('countdown', endtime);
+
+// yandex maps
+ymaps.ready(init);
+
+function init() {
+    let contactsMap = new ymaps.Map('contacts-map', {
+        center: [55.721356, 37.611387],
+        zoom: 10
+    });
+
+    myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [55.671309, 37.583955]
+        },
+    }, {
+        preset: 'islands#blackStretchyIcon',
+        iconColor: '#0095b6'
+    })
+
+    contactsMap.geoObjects
+        .add(myGeoObject)
+}
+
+// arrow
+
+let goTopBtn = document.querySelector('.back-to-top');
+
+window.addEventListener('scroll', trackScroll);
+goTopBtn.addEventListener('click', backToTop);
+
+function trackScroll() {
+    let scrolled = window.scrollY;
+    let coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        goTopBtn.classList.add('back-to-top_show');
+    } else if (scrolled < coords) {
+        goTopBtn.classList.remove('back-to-top_show');
+    }
+}
+
+function backToTop() {
+    if (window.scrollY > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 0);
+    }
+}
